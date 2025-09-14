df88ce6f6eda27bf7d1d01121c1c9444b27a9011
Richard Brearton <richardbrearton@gmail.com>
2025-09-14 22:10:21 +0100

    Minor lints

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@ int main(void) {
     // Initialize the window.
     const int screenWidth = 1280;
     const int screenHeight = 720;
+    const int target_fps = 60;
     InitWindow(screenWidth, screenHeight, "Yvium Game");
 
-    SetTargetFPS(60);
+    SetTargetFPS(target_fps);
 
     // Main game loop.
     while (!WindowShouldClose()) {
@@ -14,13 +15,9 @@ int main(void) {
 
         // Draw everything.
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
-
         DrawText("Welcome to Yvium!", screenWidth / 2 - 120, screenHeight / 2 - 20, 20, DARKGRAY);
-
         DrawText("Press ESC to exit", screenWidth / 2 - 80, screenHeight / 2 + 20, 16, GRAY);
-
         EndDrawing();
     }
 

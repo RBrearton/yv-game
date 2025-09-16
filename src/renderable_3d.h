45c467c34c9d9efe4c9bc740942f8037d03cecbf
Richard Brearton <richardbrearton@gmail.com>
2025-09-16 22:25:17 +0100

    Added a simple modeltype concept

--- src/renderable_3d.h
+++ src/renderable_3d.h
@@ -2,10 +2,21 @@
 
 #include <raylib.h>
 
+// Model type
+// ----------
+//
+// A representation of a 3D model.
+typedef enum {
+    CAPSULE,
+} yv_ModelType;
+
 // Renderable3D
 // ------------
 //
 // A representation of a 3D renderable object.
 typedef struct {
-    Model model;
-} Renderable3D;
+    yv_ModelType modelType;
+    Vector3 position;
+    Vector3 rotation;
+    Vector3 scale;
+} yv_Renderable3D;

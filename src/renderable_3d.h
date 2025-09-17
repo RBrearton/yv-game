56e8c06091ccddf6110a2f8362ca9e989c62d725
Richard Brearton <richardbrearton@gmail.com>
2025-09-17 21:46:07 +0100

    Added simple initialization functions

--- src/renderable_3d.h
+++ src/renderable_3d.h
@@ -9,6 +9,7 @@
 // A representation of a 3D model.
 typedef enum {
     CAPSULE,
+    CUBE,
 } yv_ModelType;
 
 // Renderable3D
@@ -25,3 +26,15 @@ typedef struct {
     // The transform of the object.
     Transform transform;
 } yv_Renderable3D;
+
+// Create cube
+// ------------
+//
+// Creates a cube renderable.
+yv_Renderable3D yv_CreateCube(size_t renderable_id, Transform transform);
+
+// Create capsule
+// --------------
+//
+// Creates a capsule renderable.
+yv_Renderable3D yv_CreateCapsule(size_t renderable_id, Transform transform);

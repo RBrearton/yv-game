80085c6f1550d1c0c8da503cbfb5ff1e8638c37b
Richard Brearton <richardbrearton@gmail.com>
2025-09-20 15:38:00 +0100

    Added some constants for simple terrain rendering

--- yv_client/src/well_known_terms.rs
+++ yv_client/src/well_known_terms.rs
@@ -1,4 +1,10 @@
 //! # Well known terms
 //! This module contains some constants and terms that are used throughout the project.
 
+use bevy::prelude::*;
+
 pub const TERRAIN_THICKNESS: f32 = 1.0;
+pub const TERRAIN_MATERIAL_COLOR_MEADOW_GRASS: Color = Color::srgb(0.3, 0.8, 0.3);
+pub const TERRAIN_MATERIAL_COLOR_MEADOW_WATER: Color = Color::srgb(0.1, 0.1, 0.9);
+pub const TERRAIN_MATERIAL_COLOR_SNOW_GRASS: Color = Color::srgb(0.9, 0.9, 0.9);
+pub const TERRAIN_MATERIAL_COLOR_SNOW_WATER: Color = Color::srgb(0.7, 0.7, 0.9);

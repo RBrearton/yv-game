b2c91751d2c7330531da203c3ce450594b3e3cd5
Richard Brearton <richardbrearton@gmail.com>
2025-09-20 10:53:42 +0100

    Made first pass at very simple main menu

--- yv_client/src/ui.rs
+++ yv_client/src/ui.rs
@@ -15,9 +15,34 @@ impl Plugin for UIPlugin {
     }
 }
 
-fn login_window(mut contexts: EguiContexts) -> Result {
-    egui::Window::new("Login").show(contexts.ctx_mut()?, |ui| {
-        ui.label("Login");
+/// Extension trait for `egui::Ui` to add some custom methods.
+pub(super) trait UiExtensions {
+    fn big_button(&mut self, text: &str) -> egui::Response;
+}
+
+impl UiExtensions for egui::Ui {
+    fn big_button(&mut self, text: &str) -> egui::Response {
+        self.add_sized(
+            [self.available_width() * 0.8, 36.0],
+            egui::Button::new(egui::RichText::new(text).size(16.0).strong()),
+        )
+    }
+}
+
+/// This system produces the main menu window.
+/// This is called login_window because the intent is, in the future, to have a full login system.
+fn login_window(mut contexts: EguiContexts, mut scene_state: ResMut<NextState<YvScene>>) -> Result {
+    let login_window = egui::Window::new("Main menu")
+        .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO)
+        .resizable(false)
+        .collapsible(false);
+
+    login_window.show(contexts.ctx_mut()?, |ui| {
+        if ui.big_button("Play").clicked() {
+            debug!("Play button clicked");
+            scene_state.set(YvScene::Game);
+        }
     });
+
     Ok(())
 }

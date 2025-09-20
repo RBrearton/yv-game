8007bcd54af0d5cb8d52b0fb25e5854d08b04089
Richard Brearton <richardbrearton@gmail.com>
2025-09-20 17:54:27 +0100

    Added some camera-related terms

--- yv_client/src/well_known_terms.rs
+++ yv_client/src/well_known_terms.rs
@@ -8,3 +8,14 @@ pub const TERRAIN_MATERIAL_COLOR_MEADOW_GRASS: Color = Color::srgb(0.3, 0.8, 0.3
 pub const TERRAIN_MATERIAL_COLOR_MEADOW_WATER: Color = Color::srgb(0.1, 0.1, 0.9);
 pub const TERRAIN_MATERIAL_COLOR_SNOW_GRASS: Color = Color::srgb(0.9, 0.9, 0.9);
 pub const TERRAIN_MATERIAL_COLOR_SNOW_WATER: Color = Color::srgb(0.7, 0.7, 0.9);
+
+pub mod camera {
+    //! Constants associated with the camera.
+    use bevy::prelude::*;
+    pub const DEFAULT_SPEED: f32 = 1.0;
+    pub const SPRINT_MODIFIER: f32 = 2.0; // Factor by which we increase speed.
+    pub const SENSITIVITY: f32 = 0.001; // How quickly we yaw and pitch.
+    pub const DEFAULT_PITCH: f32 = 0.0;
+    pub const DEFAULT_YAW: f32 = 0.0;
+    pub const DEFAULT_POSITION: Vec3 = Vec3::ZERO;
+}

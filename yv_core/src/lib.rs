166f97fa11383b9b037477da897c5b289e93f276
Richard Brearton <richardbrearton@gmail.com>
2025-09-20 11:44:49 +0100

    Added first pass at some terrain logic

--- yv_core/src/lib.rs
+++ yv_core/src/lib.rs
@@ -1,1 +1,4 @@
+pub mod terrain;
+pub mod well_known_terms;
+
 pub fn hello_world() {}
---
+++ yv_core/src/terrain.rs
@@ -0,0 +1,109 @@
+use bevy::prelude::*;
+use hashbrown::HashMap;
+
+use crate::well_known_terms::TERRAIN_CHUNK_SIZE;
+
+pub struct TerrainPlugin;
+
+impl Plugin for TerrainPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            add_terrain_chunks.run_if(on_event::<AddTerrainChunk>),
+        );
+        app.add_systems(
+            Update,
+            remove_terrain_chunks.run_if(on_event::<RemoveTerrainChunk>),
+        );
+    }
+}
+
+fn add_terrain_chunks(
+    mut commands: Commands,
+    mut terrain_chunks: ResMut<TerrainChunks>,
+    mut chunk_events: EventReader<AddTerrainChunk>,
+) {
+    for event in chunk_events.read() {
+        // Spawn the chunk, and add its ID to the TerrainChunks resource.
+        let new_chunk_id = commands.spawn(event.get_chunk()).id();
+        terrain_chunks.chunks.insert(event.index, new_chunk_id);
+    }
+}
+
+fn remove_terrain_chunks(
+    mut commands: Commands,
+    mut terrain_chunks: ResMut<TerrainChunks>,
+    mut chunk_events: EventReader<RemoveTerrainChunk>,
+) {
+    for event in chunk_events.read() {
+        if let Some(entity_id) = terrain_chunks.chunks.remove(&event.index) {
+            commands.entity(entity_id).despawn();
+        }
+    }
+}
+
+/// The resource that contains references to all terrain chunks.
+#[derive(Resource)]
+struct TerrainChunks {
+    chunks: HashMap<TerrainIndex, Entity>,
+}
+
+/// The event that, when fired, adds a terrain chunk to the world.
+#[derive(Event, Clone, Debug)]
+pub struct AddTerrainChunk {
+    index: TerrainIndex,
+    chunk_type: ChunkType,
+    biome: Biome,
+}
+impl AddTerrainChunk {
+    fn get_chunk(&self) -> TerrainChunk {
+        TerrainChunk {
+            index: self.index,
+            chunk_type: self.chunk_type,
+            biome: self.biome,
+        }
+    }
+}
+
+/// The event that, when fired, removes a terrain chunk from the world.
+#[derive(Event, Debug, Clone)]
+pub struct RemoveTerrainChunk {
+    index: TerrainIndex,
+}
+
+/// A 2D index that uniquely specifies a chunk of terrain.
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+pub struct TerrainIndex {
+    x: i32,
+    y: i32,
+}
+impl TerrainIndex {
+    /// Get the terrain index from a 2D position.
+    pub fn from_position(x: f32, y: f32) -> Self {
+        Self {
+            x: (x / TERRAIN_CHUNK_SIZE).floor() as i32,
+            y: (y / TERRAIN_CHUNK_SIZE).floor() as i32,
+        }
+    }
+}
+
+/// The type of terrain in a chunk.
+#[derive(Clone, Debug, Copy)]
+pub enum ChunkType {
+    Grass,
+    Water,
+}
+
+/// The biome of a chunk.
+#[derive(Clone, Debug, Copy)]
+pub enum Biome {
+    Meadow,
+    Snow,
+}
+
+#[derive(Component, Clone, Debug)]
+struct TerrainChunk {
+    index: TerrainIndex,
+    chunk_type: ChunkType,
+    biome: Biome,
+}

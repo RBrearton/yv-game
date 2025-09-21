6e78621230c07c5f4fa6ded84b2cccb830559aac
Richard Brearton <richardbrearton@gmail.com>
2025-09-21 20:42:32 +0100

    Fixed default values

--- yv_core/src/well_known_terms.rs
+++ yv_core/src/well_known_terms.rs
@@ -1,11 +1,11 @@
 //! # Well known terms
 //! This module contains some constants and terms that are used throughout the project.
 
-const TERRAIN_SAND_EPSILON: f32 = 0.1;
+const TERRAIN_SAND_EPSILON: f32 = 0.05;
 pub const TERRAIN_CHUNK_SIZE: f32 = 2.0;
 pub const TERRAIN_CONFIG_SEED: u32 = 42;
 pub const TERRAIN_WATER_CUTOFF: f32 = 0.3;
 pub const TERRAIN_SAND_CUTOFF: f32 = TERRAIN_WATER_CUTOFF + TERRAIN_SAND_EPSILON;
-pub const TERRAIN_CHUNK_VARIATION_SCALE: f32 = 0.1;
-pub const TERRAIN_BIOME_VARIATION_SCALE: f32 = 0.1;
+pub const TERRAIN_CHUNK_VARIATION_SCALE: f32 = 15.0;
+pub const TERRAIN_BIOME_VARIATION_SCALE: f32 = 15.0;
 pub const TERRAIN_BIOME_MEADOW_CUTOFF: f32 = 0.8;

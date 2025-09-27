87a0d59dacbe4ba79d55f69918ed72fd658467d5
Richard Brearton <richardbrearton@gmail.com>
2025-09-27 22:20:24 +0100

    Updated default number of shadow cascades

--- yv_client/src/lighting/well_known_terms.rs
+++ yv_client/src/lighting/well_known_terms.rs
@@ -1,4 +1,4 @@
 pub(super) const DEFAULT_SHADOW_DEPTH_BIAS: f32 = 0.1;
 pub(super) const DEFAULT_SHADOW_NORMAL_BIAS: f32 = 0.1;
 pub(super) const DEFAULT_SHADOW_MAP_NEAR_Z: f32 = 0.3;
-pub(super) const DEFAULT_NUM_SHADOW_CASCADES: usize = 1;
+pub(super) const DEFAULT_NUM_SHADOW_CASCADES: usize = 3;

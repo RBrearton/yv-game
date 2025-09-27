41ba20803687ea82216018d615c87d69f444290d
Richard Brearton <richardbrearton@gmail.com>
2025-09-27 13:14:35 +0100

    Added some constants related to colouring

--- yv_client/src/well_known_terms.rs
+++ yv_client/src/well_known_terms.rs
@@ -1,19 +1,30 @@
 //! # Well known terms
 //! This module contains some constants and terms that are used throughout the project.
 
-use bevy::prelude::*;
 use yv_core::well_known_terms::TERRAIN_CHUNK_SIZE;
 
 pub const TERRAIN_MESH_THICKNESS: f32 = 1.0;
 const TERRAIN_EPSILON: f32 = 0.1;
 pub const TERRAIN_MESH_WIDTH: f32 = TERRAIN_CHUNK_SIZE - TERRAIN_EPSILON;
-pub const TERRAIN_MATERIAL_COLOR_MEADOW_GRASS: Color = Color::srgb(0.3, 0.8, 0.3);
-pub const TERRAIN_MATERIAL_COLOR_MEADOW_WATER: Color = Color::srgb(0.1, 0.1, 0.9);
-pub const TERRAIN_MATERIAL_COLOR_MEADOW_SAND: Color = Color::srgb(0.99, 0.93, 0.44);
-pub const TERRAIN_MATERIAL_COLOR_SNOW_GRASS: Color = Color::srgb(0.9, 0.9, 0.9);
-pub const TERRAIN_MATERIAL_COLOR_SNOW_SAND: Color =
-    Color::srgb(210.0 / 255.0, 170.0 / 255.0, 109.0 / 255.0);
-pub const TERRAIN_MATERIAL_COLOR_SNOW_WATER: Color = Color::srgb(0.7, 0.7, 0.9);
+
+pub mod materials {
+    pub mod actor {
+        use bevy::prelude::*;
+        pub const TREE_TRUNK_COLOR: Color = Color::srgb(0.4, 0.8, 0.3);
+        pub const TREE_TOP_COLOR: Color = Color::srgb(0.4, 0.8, 0.3);
+        pub const HUMAN_COLOR: Color = Color::WHITE;
+    }
+
+    pub mod terrain {
+        use bevy::prelude::*;
+        pub const MEADOW_GRASS_COLOR: Color = Color::srgb(0.3, 0.8, 0.3);
+        pub const MEADOW_WATER_COLOR: Color = Color::srgb(0.1, 0.1, 0.9);
+        pub const MEADOW_SAND_COLOR: Color = Color::srgb(0.99, 0.93, 0.44);
+        pub const SNOW_GRASS_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+        pub const SNOW_SAND_COLOR: Color = Color::srgb(210.0 / 255.0, 170.0 / 255.0, 109.0 / 255.0);
+        pub const SNOW_WATER_COLOR: Color = Color::srgb(0.7, 0.7, 0.9);
+    }
+}
 
 pub mod camera {
     //! Constants associated with the camera.

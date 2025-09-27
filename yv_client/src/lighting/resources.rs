110be0810711a55a90960ae9db90daa226009785
Richard Brearton <richardbrearton@gmail.com>
2025-09-27 22:16:26 +0100

    Added shadow map cascades to the lighting config

--- yv_client/src/lighting/resources.rs
+++ yv_client/src/lighting/resources.rs
@@ -6,6 +6,7 @@ use crate::lighting::well_known_terms::*;
 pub struct LightingConfig {
     pub shadows_enabled: bool,
     pub affect_lightmapped_mesh_diffuse: bool,
+    pub num_shadow_cascades: usize,
     pub shadow_map_near_z: f32,
     pub shadow_depth_bias: f32,
     pub shadow_normal_bias: f32,
@@ -15,6 +16,7 @@ impl Default for LightingConfig {
         Self {
             shadows_enabled: true,
             affect_lightmapped_mesh_diffuse: true,
+            num_shadow_cascades: DEFAULT_NUM_SHADOW_CASCADES,
             shadow_map_near_z: DEFAULT_SHADOW_MAP_NEAR_Z,
             shadow_depth_bias: DEFAULT_SHADOW_DEPTH_BIAS,
             shadow_normal_bias: DEFAULT_SHADOW_NORMAL_BIAS,

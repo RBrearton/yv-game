d29429c020f854704bf4b6c2f94b03a24ab56ce1
Richard Brearton <richardbrearton@gmail.com>
2025-09-30 08:30:14 +0100

    Updated collider mod

--- yv_core/src/actor/mod.rs
+++ yv_core/src/actor/mod.rs
@@ -1,9 +1,8 @@
-pub mod collider_info;
+pub mod collider;
 pub mod components;
 pub mod events;
 pub mod plugin;
 pub mod systems;
 
-pub use collider_info::*;
 pub use components::*;
 pub use events::*;

cafd2612af9819acdd5a4073a5568cfa4918fb5a
Richard Brearton <richardbrearton@gmail.com>
2025-10-24 21:30:26 +0100

    Added simple describable trait

---
+++ yv_core/src/traits/describable.rs
@@ -0,0 +1,8 @@
+/// # Describable
+/// The trait that must be implemented by anything that can be described.
+pub trait Describable {
+    /// # Description
+    /// Returns the description of the object.
+    /// This is used extensively by the client to produce things like tooltips, hover text, etc.
+    fn description(&self) -> String;
+}
--- yv_core/src/traits/mod.rs
+++ yv_core/src/traits/mod.rs
@@ -1,7 +1,9 @@
 mod actor_like;
+mod describable;
 mod has_placement;
 mod identifiable;
 
 pub use actor_like::ActorLike;
+pub use describable::Describable;
 pub use has_placement::HasPlacement;
 pub use identifiable::Identifiable;

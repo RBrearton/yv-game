6de9fd695427a0e3d2fdcde5e2d5795844e3c5bc
Richard Brearton <richardbrearton@gmail.com>
2025-10-25 09:42:26 +0100

    Added simple HasStats trait

---
+++ yv_core/src/traits/has_stats.rs
@@ -0,0 +1,13 @@
+use crate::prelude::*;
+
+/// # Has stats
+/// The trait that must be implemented by anything that has stats.
+pub trait HasStats {
+    /// # Stats
+    /// Returns a reference to the stats of the object.
+    fn stats(&self) -> &Stats;
+
+    /// # Get stat
+    /// Gets the stat with the given type.
+    fn get_stat(&self, stat_type: StatType) -> Stat;
+}
--- yv_core/src/traits/mod.rs
+++ yv_core/src/traits/mod.rs
@@ -2,6 +2,7 @@ mod actor_like;
 mod describable;
 mod has_display_name;
 mod has_placement;
+mod has_stats;
 mod identifiable;
 mod ownable;
 
@@ -9,5 +10,6 @@ pub use actor_like::ActorLike;
 pub use describable::Describable;
 pub use has_display_name::HasDisplayName;
 pub use has_placement::HasPlacement;
+pub use has_stats::HasStats;
 pub use identifiable::Identifiable;
 pub use ownable::Ownable;

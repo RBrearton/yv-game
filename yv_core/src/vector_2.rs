1ddd60931d07bffabc672990a43078efb97acb9d
Richard Brearton <richardbrearton@gmail.com>
2025-10-26 17:53:58 +0000

    Simplified distance_to_sq implementation

--- yv_core/src/vector_2.rs
+++ yv_core/src/vector_2.rs
@@ -1,5 +1,8 @@
 use crate::prelude::*;
 
+/// # Vector2
+/// A 2D vector.
+/// This is a simple struct that contains two floating point numbers.
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Vector2 {
     pub x: f32,
@@ -16,6 +19,6 @@ impl Vector2 {
     /// # Distance to squared
     /// Returns the squared distance between the vector and the given position.
     pub fn distance_to_sq(&self, other: Vector2) -> f32 {
-        (self.x - other.x).powi(2) + (self.y - other.y).powi(2)
+        (self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y)
     }
 }

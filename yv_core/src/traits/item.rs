45455a24fab436799ddf01dbf5f4be48c4bce0a7
Richard Brearton <richardbrearton@gmail.com>
2025-10-26 08:28:17 +0000

    Added an is_stackable method to Item trait

--- yv_core/src/traits/item.rs
+++ yv_core/src/traits/item.rs
@@ -1,5 +1,7 @@
 use crate::prelude::*;
 
+/// # Item
+/// This trait defines what we expect from all items in the game.
 pub trait Item:
     Ownable
     + Describable
@@ -11,4 +13,8 @@ pub trait Item:
     + Serialize
     + for<'a> Deserialize<'a>
 {
+    /// # Is stackable
+    /// Whether this item can be stacked in an inventory, or not.
+    /// Items that can stack can be stacked infinitely - the only bottleneck is the resultant mass.
+    fn is_stackable() -> bool;
 }

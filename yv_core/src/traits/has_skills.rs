2f793d1eb83eed7199d8fc08104a480c44986e17
Richard Brearton <richardbrearton@gmail.com>
2025-11-01 22:17:31 +0000

    Made HasSkills much easier to implement

--- yv_core/src/traits/has_skills.rs
+++ yv_core/src/traits/has_skills.rs
@@ -7,19 +7,25 @@ pub trait HasSkills {
     /// Returns a reference to the skills of the object.
     fn skills(&self) -> &Skills;
 
-    /// # Get skill
-    /// Gets the skill with the given type.
-    fn get_skill(&self, skill_type: SkillType) -> Skill;
-
-    /// # Get skill level
-    /// Gets the level of the skill with the given type.
-    fn get_skill_level(&self, skill_type: SkillType) -> i32;
-
-    /// # Get skill xp
-    /// Gets the xp of the skill with the given type.
-    fn get_skill_xp(&self, skill_type: SkillType) -> i32;
-
     /// # Skills mut
     /// Returns a mutable reference to the skills of the object.
     fn skills_mut(&mut self) -> &mut Skills;
+
+    /// # Get skill
+    /// Gets the skill with the given type.
+    fn get_skill(&self, skill_type: SkillType) -> Skill {
+        self.skills().get_skill(skill_type)
+    }
+
+    /// # Get skill level
+    /// Gets the level of the skill with the given type.
+    fn get_skill_level(&self, skill_type: SkillType) -> i32 {
+        self.get_skill(skill_type).level()
+    }
+
+    /// # Get skill xp
+    /// Gets the xp of the skill with the given type.
+    fn get_skill_xp(&self, skill_type: SkillType) -> i32 {
+        self.get_skill(skill_type).xp
+    }
 }

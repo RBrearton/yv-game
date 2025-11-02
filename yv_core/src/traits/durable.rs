1a57bdf7b0dfd04a561ca9e8c6052c65a9c074fa
Richard Brearton <richardbrearton@gmail.com>
2025-11-02 12:59:18 +0000

    Made Durable trait easier to implement

--- yv_core/src/traits/durable.rs
+++ yv_core/src/traits/durable.rs
@@ -1,21 +1,39 @@
+use crate::prelude::*;
+
 /// # Durable
 /// The trait that must be implemented by anything that has durability.
 pub trait Durable {
+    /// # Durability
+    /// Returns the durability of the object.
+    fn durability(&self) -> &Durability;
+
+    /// # Durability mut
+    /// Returns a mutable reference to the durability of the object.
+    fn durability_mut(&mut self) -> &mut Durability;
+
     /// # Current durability
     /// Returns the current durability of the object.
-    fn current_durability(&self) -> u8;
+    fn current_durability(&self) -> u8 {
+        self.durability().current_durability()
+    }
 
     /// # Max durability
     /// Returns the max durability of the object.
-    fn max_durability(&self) -> u8;
+    fn max_durability(&self) -> u8 {
+        self.durability().max_durability()
+    }
 
     /// # Decrease durability
     /// Decreases the durability of the object.
-    fn decrease_durability(&mut self);
+    fn decrease_durability(&mut self) {
+        self.durability_mut().decrease_durability()
+    }
 
     /// # Restore durability
     /// Restores the durability of the object to the max durability.
-    fn restore_durability(&mut self);
+    fn restore_durability(&mut self) {
+        self.durability_mut().restore_durability()
+    }
 
     /// # Is broken
     /// Returns true if the durability is 0.

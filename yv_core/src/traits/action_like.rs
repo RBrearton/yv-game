27c670010929fdf28800e0579bd670d14c1bc90f
Richard Brearton <richardbrearton@gmail.com>
2025-11-08 13:25:56 +0000

    Moved HasPerformer and HasTarget back into ActionLike

--- yv_core/src/traits/action_like.rs
+++ yv_core/src/traits/action_like.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 /// # Action like
 /// The trait that must be implemented by anything that can be acted upon.
 #[delegatable_trait]
-pub trait ActionLike: Identifiable + Describable + HasPerformer + HasTarget {
+pub trait ActionLike: Identifiable + Describable {
     /// # Range
     /// Returns the range of the action.
     fn range(&self) -> f32;
@@ -19,5 +19,15 @@ pub trait ActionLike: Identifiable + Describable + HasPerformer + HasTarget {
 
     /// # On completion
     /// The method called when the action finishes.
-    fn on_completion(&self, world: &mut World);
+    fn on_completion(&self, world: &World) -> Commands;
+
+    /// # Target
+    /// The identity of the target of the action.
+    /// If this is None, then the action is a self-targeting action. and the performer is the
+    /// target.
+    fn target(&self) -> Option<&Identity>;
+
+    /// # Performer
+    /// Returns the performer of the object.
+    fn performer(&self) -> &Identity;
 }

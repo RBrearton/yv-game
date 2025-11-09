a746607d449cccb7b89ed3fff5e2d5b60c9575f2
Richard Brearton <richardbrearton@gmail.com>
2025-11-09 09:55:58 +0000

    Made SkillLevels a bit more ergonomic to work with

--- yv_core/src/skill_levels.rs
+++ yv_core/src/skill_levels.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 /// # Skill levels
 /// A struct that mirrors the Skills struct, but instead of storing xp, it stores levels.
-#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct SkillLevels {
     pub strength: u8,
     pub mining: u8,
@@ -21,6 +21,32 @@ impl From<Skills> for SkillLevels {
     }
 }
 
+impl From<&Skills> for SkillLevels {
+    fn from(value: &Skills) -> Self {
+        Self {
+            strength: value.strength.level() as u8,
+            mining: value.mining.level() as u8,
+            woodcutting: value.woodcutting.level() as u8,
+            magic: value.magic.level() as u8,
+        }
+    }
+}
+
+impl IntoIterator for SkillLevels {
+    type Item = (SkillType, u8);
+    type IntoIter = std::array::IntoIter<(SkillType, u8), 4>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let arr = [
+            (SkillType::Strength, self.strength),
+            (SkillType::Mining, self.mining),
+            (SkillType::Woodcutting, self.woodcutting),
+            (SkillType::Magic, self.magic),
+        ];
+        arr.into_iter()
+    }
+}
+
 impl SkillLevels {
     /// # New
     /// Creates a new set of skill levels with the given levels.
@@ -31,5 +57,14 @@ impl SkillLevels {
             woodcutting,
             magic,
         }
+    }
+
+    /// # Are all greater than or equal to
+    /// Returns whether all the skill levels are greater than or equal to the given level.
+    pub fn are_all_greater_than_or_equal_to(&self, skill_levels: &SkillLevels) -> bool {
+        self.strength >= skill_levels.strength
+            && self.mining >= skill_levels.mining
+            && self.woodcutting >= skill_levels.woodcutting
+            && self.magic >= skill_levels.magic
     }
 }
